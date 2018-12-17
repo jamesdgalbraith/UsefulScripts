748c56d30d1e021309d52f8bfd74de28b311bca7
jamesdgalbraith <galbraithenator@gmail.com>
2018-12-17 17:09:47 +0100

    Added irfToBed.py. Updated readme.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 An assorted collection of useful scripts for bioinformatic purposes
+
+trfToGff.py converts the -ngs output of Tandem Repeat Finder to GFF format. For example:
+
+python trfToGff.py <trf_output> > trf.gff
+
+
+irfRoBed.py converts the -ngs output of Inverted Repeat Finder to BED format. For example:
+python irfToBed.py <irf_output> > irf.bed
+
---
+++ irfToBed.py
@@ -0,0 +1,19 @@
+# adapted from https://gist.github.com/lexnederbragt/3689ee2301493c34c8ab#file-trf2gff-py
+
+# Library  needed for dealing with system
+import sys
+
+# Open file
+with open(sys.argv[1]) as infile:
+  # All below loops over file
+  for line in infile:
+    # Split input into columns based on spaces
+    ele = line.strip().split(" ")
+    # Grab sequence name from lines beginning with "@"
+    if line.startswith('@'):
+      seq_name = ele[0][1:]
+    # Print sequence name and select other columns for other lines
+    else:
+      [lstart, lstop, lperiod, rstart, rstop, rperiod, loop, pcmatches, pcindels, score, pcAT, pcGC, pcATpair, pcGCpair, pcGTpair, ctr2, av_ctr2, lseq, rseq] = ele
+      bed_line = [seq_name, lstart, rstop]
+      print '\t'.join(bed_line)
